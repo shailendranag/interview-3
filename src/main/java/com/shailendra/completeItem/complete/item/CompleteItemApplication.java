152c6f69b021c90866a906a43803606c12890a7d
nag <sailunag18@gmail.com>
2022-03-18 15:59:16 +0530

    Initial commit #2

---
+++ src/main/java/com/shailendra/completeItem/complete/item/CompleteItemApplication.java
@@ -0,0 +1,13 @@
+package com.shailendra.completeItem.complete.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteItemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shailendra/completeItem/complete/item/CompleteItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shailendra.completeItem.complete.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
